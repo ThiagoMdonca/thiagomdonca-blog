5eb9d57268fc5a42bba4f4a2624ddc2c76a65437
thiagoMdonca <thiagolimamdonca@gmail.com>
2021-01-24 23:10:28 -0300

    alura.js codigo fonte tv e dev soutinho

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+import GlobalStyle from '../components/GlobalStyle'
+
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
 }
-
 export default function App({ Component, pageProps }) {
   return (
     <>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
+import React from 'react';
+import Link from 'next/link';
 import styled from 'styled-components'
+import Footer from '../components/Footer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const SubTitle = styled.h2`
+ background-color: var(--primary);
+ color: white;
+ display: inline-block;
+ padding: 5px;
 `
+export default function Home(props) {
+  return (
+    <div>
+      <header className="headerContainer">
+        <img src={props.avatar_url} />
+        <Link href="/sobre">
+          <a>
+            <h1>Thiago blog</h1>
+          </a>
+        </Link>
+      </header>
+      <section className="postContainer">
+        <SubTitle>Posts</SubTitle>
+        <article className="postContainer_post">
+          <a href="/" >
+            Alura js 01 blog;
+          </a>
+          <p>
+            O que aprendi com o  CDFTV e a Alura nesse episódio show!
+          </p>
+        </article>
+      </section>
+      <section className="postContainer">
+        <SubTitle> Repositorios Favoritos</SubTitle>
+        {props.repos.map((project, key) => (
+          <article key={key} className="postContainer__post">
+            <a href={`${props.url_github}${props.username}/${project.repo}`} target='blank' >
+              {project.repo}
+            </a>
+            <p>{project.description}</p>
+          </article>
+        ))}
+      </section>
+      <Footer />
+    </div>
+  )
 
-export default function Home() {
-  return <Title>My page</Title>
+}
+
+export async function getStaticProps() {
+  const githubResponse = await fetch('https://api.github.com/users/ThiagoMdonca')
+    .then(res => res.json());
+
+  const repos = await fetch('https://gh-pinned-repos.now.sh/?username=ThiagoMdonca')
+    .then(res => res.json());
+  return {
+    props: {
+      avatar_url: githubResponse.avatar_url,
+      username: githubResponse.login,
+      repos,
+    }
+  }
 }
